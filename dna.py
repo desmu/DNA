728ec3f5e1b3c12f739ce139375bac5e7ab1f6c1
desmu <90912716+desmu@users.noreply.github.com>
2021-10-14 16:42:42 +0100

    Update dna.py

--- dna.py
+++ dna.py
@@ -15,6 +15,7 @@ for i in range(1,len(x)):#on passe une fois dans la boucle pour chaque groupe de
     nb = 0#nombre d'itération consécutive d'une séquence type dans la séquence analysée
     prec = 0#indice de la séquence précédente
     a = 0#indice de la position de la boucle dans l'analyse de la grande séquence
+    #ici while est chisi car for in range avance de 1 en un sans tenir compte des modifications de a dans l'action de la boucle
     while a < len(seq):#on scanne toute la séquence à la recherche du nombre maximal consécutif de la séquence type "i" que l'on est entrain de chercher
         a = seq.find(x[i], a)#on retourne la position de l'itération suivante de la séquence type "i" recherchée en partant de "a"
         if a == -1: #retourne -1 si non trouvé/si aucune itération de la séquence cherchée à partir de "a" n'est trouvée
